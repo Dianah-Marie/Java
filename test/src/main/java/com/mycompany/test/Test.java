5410e2a776537edc9f0ab38ed638706c6ee8b3fe
Dianah-Marie <144867949+Dianah-Marie@users.noreply.github.com>
2024-03-31 19:15:06 +0800

    Add files via upload

---
+++ test/src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,11 @@
+package com.mycompany.test;
+public class Test {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+        
+        System.out.println(x + y);
+        
+    }
+}
